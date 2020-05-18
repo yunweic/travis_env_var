129b2a9d2c382d3ad172b13d7f7cbb1415ba7d2c
Ubuntu <graphen@FraudDevelop.pvhj15gifigubhsyj3wwvtzacd.bx.internal.cloudapp.net>
2020-05-18 19:54:22 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
+    console.log("Microservices rock!");
+    await sleep(5000);
+  }
+}
+
+main();
